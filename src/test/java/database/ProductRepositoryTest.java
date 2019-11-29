06827b9ba17820ad7aeaf28f302fd3a01bc4112e
denisu76 <denisu76@gmail.com>
2019-11-29 13:28:15 +0200

    nr3

---
+++ src/test/java/database/ProductRepositoryTest.java
@@ -0,0 +1,28 @@
+package database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+
+    @Test
+    void findAll1() {
+        ProductRepository repository = new ProductRepository();
+        assertNotNull(repository.findAll());
+
+    }
+/*
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+*/
+}
